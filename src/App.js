da3b9f756317e2ce8e7abf5b4518f4714e13a8e4
StarBurnStream <1106640847@qq.com>
2018-03-28 22:13:26 -0400

    project2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Professorpage from './Professorpage';
+import Namepage from './Namepage';
+import CRNpage from './CRNpage';
+import Coursepage from './Coursepage';
+import Mainpage from './Mainpage';
+import Courserecommendationpage from './Courserecommendationpage';
+import Help from './Help';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+	  <div>
+	  <Route exact path='/' render = {() => (
+	  <MuiThemeProvider>
+		<Mainpage />
+	  </MuiThemeProvider>
+	  )}/>
+	  <Route path='/help' render = {() => (
+	  <MuiThemeProvider>
+		<Help />
+	  </MuiThemeProvider>
+	  )}/>
+	  <Route path='/searchcourse' render = {() => (
+	  <MuiThemeProvider>
+		<Coursepage />
+	  </MuiThemeProvider>
+	  )}/>
+	  <Route path='/searchbycrn' render = {() => (
+	  <MuiThemeProvider>
+		<CRNpage />
+	  </MuiThemeProvider>
+	  )}/>
+	  <Route path='/searchbyname' render = {() => (
+	  <MuiThemeProvider>
+		<Namepage />
+	  </MuiThemeProvider>
+	  )}/>
+	  <Route path='/searchbyprofessor' render = {() => (
+	  <MuiThemeProvider>
+		<Professorpage />
+	  </MuiThemeProvider>
+	  )}/>
+	  <Route path='/courserecommendation' render = {() => (
+	  <MuiThemeProvider>
+		<Courserecommendationpage />
+	  </MuiThemeProvider>
+	  )}/>
+	  </div>
     );
   }
 }
